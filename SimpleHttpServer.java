4a44e6c84382f38f737e01f1e6a697fc3a4f280f
957419821 <957419821@qq.com>
2018-07-08 00:11:56 +0800

    add some annotations

--- SimpleHttpServer.java
+++ SimpleHttpServer.java
@@ -7,18 +7,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SimpleHttpServer {
+    // 设置该服务监听的端口
     private final static int TCP_PORT = 9090;
 
  	   public static void main(String[] args) throws IOException {
+		// 创建 server socket，关于该类的详细介绍参考
+		//     https://docs.oracle.com/javase/7/docs/api/
         	ServerSocket ss = new ServerSocket(TCP_PORT);
+		// 监听到请求之后，创建socket
 	        Socket socket = ss.accept();
+
+		// 获取请求
+		//     从socke创建InputStream
+		//     创建InputStreamReader
+		//     创建BufferReader
 	        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		//     从Buffer中获取HTTP请求的内容，并在终端显示出来
 	        String buffer = null;
-	        while ((buffer = br.readLine()) != null && !s.equals("")) {
+	        while ((buffer = br.readLine()) != null && !buffer.equals("")) {
 	            System.out.println(buffer);
 	        }
-	
+
+		// 返回信息
+		//     通过socket创建OutputStream
+		//     创建OutputStreamWriter
+		//     创建BufferWriter
 	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		//     在Buffer中写入待返回的信息
 	        bw.write("HTTP/1.1 200 OK\n");
 	        bw.write("Content-Type: text/html; charset=UTF-8\n\n");
 	        bw.write(
@@ -31,11 +46,15 @@ public class SimpleHttpServer {
 	                + "</body>\n" 
 			+ "</html>\n"
 		);
+		//     将Buffer中的数据 *发送* 出去，之后清空Buffer
 	        bw.flush();
+		// 关闭BufferWriter
 	        bw.close();
-	
+		// 关闭BufferReader
 	        br.close();
+		// 关闭socket
 	        socket.close();
+		// 关闭ServerSocket
 	        ss.close();
 	    }
 }
